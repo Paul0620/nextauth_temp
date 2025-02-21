47f04324ce216457e38ac34eaaad4a586c350405
paul <dlrhkdghsp@gmail.com>
2025-02-21 23:02:16 +0900

    [FIX] 프로젝트 제설정

--- src/app/auth/layout.tsx
+++
@@ -1,5 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex justify-center items-center bg-black text-white h-screen">{children}</div>
-  )
-}
--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { auth as middleware } from '@/lib/auth'
-
-// 인증이 필요하지 않은 경로 설정
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth).*)'],
-}
