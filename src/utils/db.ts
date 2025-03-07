fc784b36d7f7ba87066cee76fbe2746ef5dda944
paul <dlrhkdghsp@gmail.com>
2025-03-07 23:43:59 +0900

    [DEV] password 추가 작업

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,22 @@
 import { prisma } from '@/lib/prisma'
+import { verifyPassword } from './password'
 
-export async function getUserFromDb(email: string, password: Promise<string>) {
-  // TODO: 암호화 작업
-  console.log(password)
-
-  return prisma.user.findUnique({
+export async function getUserFromDb(email: string, password: string) {
+  // 1. 사용자의 정보를 DB에서 조회
+  const user = await prisma.user.findUnique({
     where: { email },
   })
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  // 2. 저장된 해시된 비밀번호와 사용자가 입력한 평문 비밀번호 비교
+  const isPasswordValid = await verifyPassword(password, user.password!)
+
+  if (!isPasswordValid) {
+    throw new Error('Invalid password')
+  }
+
+  return user
 }
